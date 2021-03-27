aa86554012494c634694deac1fdb2bb708dcf3ce
AleksunderVolkov <alexander.volkov@rochev.studio>
2021-03-28 01:26:19 +0300

    Added numbers and seconds marks

--- Clock/Clock View/ClockMarks.swift
+++ Clock/Clock View/ClockMarks.swift
@@ -3,17 +3,18 @@ import SwiftUI
 
 struct ClockMarks: View {
     
-    private let relativeLength: CGFloat = 0.05
+    var relativeLength: CGFloat = 0.01
     
     var body: some View {
         GeometryReader { geometry in
-            ForEach(0..<12) { n in
+            ForEach(0..<60) { n in
                 Path { path in
+                    let length = n % 5 == 0 ? self.relativeLength * 5 : self.relativeLength
                     path.move(to: CGPoint(x: geometry.size.width / 2, y: 0))
-                    path.addLine(to: CGPoint(x: geometry.size.width / 2, y: self.relativeLength * geometry.size.height))
+                    path.addLine(to: CGPoint(x: geometry.size.width / 2, y: length * geometry.size.height))
                 }
                 .stroke(Color.secondary)
-                .rotationEffect(Angle(degrees: Double(n) * 360 / 12))
+                .rotationEffect(Angle(degrees: Double(n) * 360 / 60))
             }
         }
     }
